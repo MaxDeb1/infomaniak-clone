3e169509154adf99d7d125698c39e94ca60f6fd0
MaxDeb1 <deb1maxime@gmail.com>
2023-06-30 19:05:13 +0200

    header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { css } from '../styled-system/css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
+      <Header />
       <div className={css({ fontSize: "2xl", fontWeight: 'bold' })}>Hello 🐼!</div>
     </>
   )
